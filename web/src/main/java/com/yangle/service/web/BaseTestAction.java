12bdaac5e85b86dad0e4f0c7d4fbfa2dd86d7a14
yishao <yishao@meili-inc.com>
2018-11-14 18:16:44 +0800

    连接池

--- web/src/main/java/com/yangle/service/web/BaseTestAction.java
+++ web/src/main/java/com/yangle/service/web/BaseTestAction.java
@@ -1,5 +1,6 @@
 package com.yangle.service.web;
 
+import com.yangle.service.biz.ProductBiz;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -25,10 +27,22 @@ public class BaseTestAction {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseTestAction.class);
 
+    @Resource
+    private ProductBiz productBiz;
+
     @RequestMapping("/status")
     @ResponseBody
     public String status(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap){
         LOGGER.info("===================ddddd");
+        return "SUCCESS";
+    }
+
+    @RequestMapping("/insert")
+    @ResponseBody
+    public String insert(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap){
+        LOGGER.info("===================insert");
+        productBiz.addNewProduct();
+
         return "SUCCESS";
     }
 
