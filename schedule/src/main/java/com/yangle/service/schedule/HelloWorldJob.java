db59ccf348272ef68ebf73e39a755aafed4f88cb
yishao <yishao@meili-inc.com>
2018-12-05 15:48:03 +0800

    日志文件配置

--- schedule/src/main/java/com/yangle/service/schedule/HelloWorldJob.java
+++ schedule/src/main/java/com/yangle/service/schedule/HelloWorldJob.java
@@ -23,7 +23,7 @@ public class HelloWorldJob implements Job{
 
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
-//        logger.info("this is a test job!");
+        logger.info("this is a test job!");
 
     }
 }
